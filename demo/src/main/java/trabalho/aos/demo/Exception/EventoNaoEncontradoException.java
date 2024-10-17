f4aecc8ccb9e6183fde886c393ca9342292b015b
Eduard0177 <131684798+Eduard0177@users.noreply.github.com>
2024-10-17 12:05:05 -0300

    Delete demo directory

--- demo/src/main/java/trabalho/aos/demo/Exception/EventoNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package trabalho.aos.demo.Exception;
-
-public class EventoNaoEncontradoException extends RuntimeException {
-  public EventoNaoEncontradoException(String message) {
-    super(message);
-  }
-}
